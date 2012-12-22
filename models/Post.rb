1144a535c2e0554e98f1a889b830fd435bbaf3f1
user <jiecenzhao@gmail.com>
2012-12-21 18:28:45 -0800

    content

--- models/Post.rb
+++ models/Post.rb
@@ -3,15 +3,50 @@
 require 'date'
 
 class Post
-	class NotFound < StandardError
+	class NotFound < StandardError; end
+
+	attr_accessor :title, :slug, :tags, :summary, :content, :repo
+
+	def initialize(args = {})
+		args.each do |x, y|
+			send "#{x}=", y
+		end
 	end
 
-	def initialize
+	def Post.load_posts
+		order = YAML.load_file "./posts/order.yml"
+		order.inject([]) do |result, line|
+			slug = line.first
+			filename = "./posts/#{slug}.md"
+      		content = File.read(filename).force_encoding "utf-8"
+
+			the_date = line.last || Time.now.strftime("%Y-%m-%d")
+			title = data['title']
+			
+			# tags = data['tags'].inject([]) do |result, tag|
+			# 	result << tag
+			# end unless data['tags'].nil?
+
+			summary = data['summary']
+			content = data['content']
+			slug = data['slug']
+			repo = data['repo']
+
+			result << (Post.new title: title, tags: tags, summary: summary, content: content, slug: slug, repo: repo)
+		end
 	end
 
+	def Post.find(slug)
+		all[slug] or raise Post::NotFound
+	end
 
 	def Post.all
-		@@all_posts ||= nil
+		@@all ||= Hash[load_posts.map { |post|
+			[post.slug, post]
+		}]
 	end
 
+	def Post.clear_posts!
+		@@all = nil
+	end
 end
