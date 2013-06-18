183495f6f84edde9be3cf7f2c57fc539177c3cbe
Stanley Cen <jiecenzhao@gmail.com>
2013-06-18 11:55:58 -0700

    Add lightbox

---
+++ public/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('a .lightbox').fancybox({
+        padding: 0,
+        openEffect: 'elastic',
+        openSpeed: 150,
+        closeEffect: 'elastic',
+        closeSpeed: 150
+    });
+});
