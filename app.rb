941c26f0337e8b69f443390a99a9e9b0cd7e6c58
Stanley Cen <jiecenzhao@gmail.com>
2013-08-22 00:01:14 -0700

    Add exception handling

--- app.rb
+++ app.rb
@@ -67,10 +67,13 @@ before do
     Post.clear_posts!
     @git = nil
   end
-  @git = @git || Git.open(Dir.pwd) unless is_heroku?
-  @heroku = @heroku || Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASS']) if is_heroku?
-  @last_commit = @last_commit || @heroku.releases("stanleycen").last['commit'] if is_heroku?
-  @the_commit = @the_commit || Octokit.commit('scen/stanleycen.com', @last_commit) if is_heroku?
+  begin
+    @git = @git || Git.open(Dir.pwd) unless is_heroku?
+    @heroku = @heroku || Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASS']) if is_heroku?
+    @last_commit = @last_commit || @heroku.releases("stanleycen").last['commit'] if is_heroku?
+    @the_commit = @the_commit || Octokit.commit('scen/stanleycen.com', @last_commit) if is_heroku?
+  rescue
+  end
 end
 
 # Error handling
