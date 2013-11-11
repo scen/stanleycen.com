f82ffb93841c983aeda1a3fd352b171fb4e0cfb6
Stanley Cen <jiecenzhao@gmail.com>
2013-11-10 23:50:02 -0800

    Add parallax effect

--- public/main.js
+++ public/main.js
@@ -1,15 +1,50 @@
-$(document).ready(function() {
-    $('a.popout-lightbox').fancybox({
-        // padding: 0,
-        openEffect: 'elastic',
-        // openSpeed: 150,
-        closeEffect: 'elastic',
-        // closeSpeed: 150,
-        closeClick: true,
-        openEasing: 'easeOutBack',
-        closingEasing: 'easeInBack',
-        helpers: {
-            // overlay: null
+var glob = {
+    PARALLAX_SPEED: 3,
+
+    resizeHomeParallax: function() {
+        $("#home").height($(window).height());
+    },
+    updateHomeParallax: function() {
+        if ($.browser.mobile) return;
+        var y = -$(window).scrollTop() / glob.PARALLAX_SPEED;
+        $("#home").css({
+            backgroundPosition: 'center ' + y + 'px',
+        });
+    },
+    updateScrollNotifier: function() {
+        if ($(window).scrollTop() != 0) {
+            $("#scroll-notifier").hide();
         }
+        else {
+            $("#scroll-notifier").show();
+        }
+    },
+    updateHomeShoutout: function() {
+        $("#home-shoutout").css({
+            top: ($(window).height() / 2) + 'px',
+            marginTop: -($("#home-shoutout").height() / 2) + 'px'
+        });
+    }
+};
+
+$(document).ready(function() {
+
+    glob.resizeHomeParallax();
+    glob.updateHomeParallax();
+    glob.updateScrollNotifier();
+    glob.updateHomeShoutout();
+    $(window).scroll(function(event) {
+        glob.updateHomeParallax();
+        glob.updateScrollNotifier();
+    });
+    $(window).resize(function(event) {
+        if (!$.browser.mobile) {
+            glob.resizeHomeParallax();
+            glob.updateHomeShoutout();
+        }
+    });
+    $(window).bind('orientationchange', function(event) {
+        glob.updateHomeShoutout();
+        glob.resizeHomeParallax();
     });
 });
