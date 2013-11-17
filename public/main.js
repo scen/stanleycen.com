f8dfd57478227fa22fa1f907db94a8eb7cb021b7
Stanley Cen <jiecenzhao@gmail.com>
2013-11-16 21:57:44 -0800

    Add responsive menu

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 var glob = {
     PARALLAX_SPEED: 3,
     SCROLL_SPEED: 500,
+    MENU_SPEED: 500,
+
+    menu_open: false,
 
     resizeHomeParallax: function() {
         $("#home").height($(window).height());
@@ -28,24 +31,37 @@ var glob = {
             top: ($(window).height() / 2) + 'px',
             marginTop: -($("#home-shoutout").height() / 2) + 'px'
         });
+    },
+    toggleNav: function() {
+        var $a = $(".navicon-button");
+        $a.find(".navicon").toggleClass('x');
+        glob.menu_open = !glob.menu_open;
+        $("#header").toggleClass('hide-nav', !glob.menu_open);
     }
 };
 
-$(document).ready(function() {
 
+$(document).ready(function() {
     glob.resizeHomeParallax();
     glob.updateParallax();
     glob.updateScrollNotifier();
     glob.updateHomeShoutout();
 
-    $("#header > nav > a").click(function(event) {
+    $("#header a").click(function(event) {
         var $a = $(this);
-        if ($a.attr('href')[0] == '#') {
+        if ($a.attr('href') != null && $a.attr('href')[0] == '#') {
+            if (glob.menu_open) {
+                glob.toggleNav();
+            }
             $('html, body').stop().animate({
                 scrollTop: $($a.attr('href')).offset().top - $("#header").height()
             }, glob.SCROLL_SPEED, 'easeInOutExpo');
             event.preventDefault();
         }
+    });
+
+    $(".navicon-button").click(function(event) {
+        glob.toggleNav();
     });
 
     $(window).scroll(function(event) {
